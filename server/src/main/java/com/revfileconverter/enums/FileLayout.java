8bd7ee5c21fbf3af511bde46021ed4feeaf1770c
ashertukde <Shertukde@gmail.com>
2024-01-18 14:58:27 -0800

    Refactored file selection on backend

--- server/src/main/java/com/revfileconverter/enums/FileLayout.java
+++ server/src/main/java/com/revfileconverter/enums/FileLayout.java
@@ -1,6 +1,13 @@
 package com.revfileconverter.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum FileLayout {
-    PERSON,
-    CAR
+    PERSON(0),
+    CAR(1);
+    private final int value;
+    FileLayout(int i) {
+        this.value = i;
+    }
 }
