196dc5a7a0a2a787d75860b78face872977b2599
Cyaorn <diepc153@gmail.com>
2024-01-24 16:50:47 -0500

    added preset file specs documentation, changed name of file layout option to 'Person with Long Data' to match the other one for Car

--- client/src/app/upload/upload.component.ts
+++ client/src/app/upload/upload.component.ts
@@ -20,12 +20,12 @@ export class UploadComponent {
   fileLayout = '';
   fileSizeError = '';
   errorMessage = '';
-  options: string[] = ['Person', 'Person Long Names', 'Car', 'Car with Long Data'];
+  options: string[] = ['Person', 'Person with Long Data', 'Car', 'Car with Long Data'];
   optionMapper: { [key: string]: string; } = {
     'Person' : 'PERSON',
     'Car' : 'CAR' ,
     'Car with Long Data' : 'CAR2',
-    'Person Long Names' : 'PERSON2'
+    'Person with Long Data' : 'PERSON2'
   };
 
 
